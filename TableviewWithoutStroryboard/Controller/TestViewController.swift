4ab2227aacad0343ea05064407f6b56914537cf8
Kirill <kirilldrozd12@gmail.com>
2022-04-04 23:58:13 +0300

    чуть редакнул

--- TableviewWithoutStroryboard/Controller/TestViewController.swift
+++ TableviewWithoutStroryboard/Controller/TestViewController.swift
@@ -13,7 +13,7 @@ struct TestViewController: View {
       List{
         Section("Разработчки") {
 
-          CellView(positions: Position.teamLead.rawValue, nickname: "Kiko")
+          CellView(image: "test", positions: Position.TeamLead.rawValue, nickname: "Kiko", link_Profile: "https://github.com/Kirill12a")
         }
       }
       .navigationTitle("Создатели")
@@ -23,23 +23,39 @@ struct TestViewController: View {
 }
 
 enum Position: String{
-  case teamLead
-  case developer
-  case design
-  case ideas
+  case TeamLead
+  case Developer
+  case Design
+  case Ideas
 }
 
+
+
+
+
 struct CellView: View {
+  @State var image:String
   @State var positions: Position.RawValue
   @State var nickname: String
+  @State var link_Profile: String
   var body: some View{
     HStack{
+      Image(uiImage: UIImage(named: image)!)
+        .resizable()
+        .cornerRadius(20)
+        .frame(width: 50, height: 50)
       Text(nickname)
+        .font(.headline)
+        .padding(.leading)
+
       Spacer()
-      Link(positions, destination: URL(string: "https://github.com/Kirill12a")!)
+      Link(positions, destination: URL(string: link_Profile)!)
     }
   }
 }
+
+
+
 
 struct TestViewController_Previews: PreviewProvider {
     static var previews: some View {
