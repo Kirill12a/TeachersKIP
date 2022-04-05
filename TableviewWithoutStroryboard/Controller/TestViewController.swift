221584f7c504018e55918c3da979eac922e62a63
Kirill <kirilldrozd12@gmail.com>
2022-04-05 11:41:52 +0300

    чуть редакнул код, и добавил новго чела и новую секцию

--- TableviewWithoutStroryboard/Controller/TestViewController.swift
+++ TableviewWithoutStroryboard/Controller/TestViewController.swift
@@ -7,20 +7,45 @@
 
 import SwiftUI
 
+
 struct TestViewController: View {
+  
     var body: some View {
       NavigationView{
       List{
         Section("Разработчки") {
-
           CellView(image: "test", positions: Position.TeamLead.rawValue, nickname: "Kiko", link_Profile: "https://github.com/Kirill12a")
+
+          CellView(image: "test4", positions: Position.Developer.rawValue, nickname: "bobermiha", link_Profile: "https://github.com/bobermiha")
+
         }
+
+        Section("Дизайн"){
+          CellView(image: "test3", positions: Position.Design.rawValue, nickname: "Shmadilubda", link_Profile: "https://github.com/Shmadilubda")
+
+          CellView(image: "test2", positions: Position.Design.rawValue, nickname: "Stefaefka", link_Profile: "https://github.com/Stefaefka")
+        }
+
+        Section("Идеи"){
+          CellView(image: "test4-1", positions: Position.Ideas.rawValue, nickname: "Madonna666", link_Profile: "https://github.com/Madonna666")
+        }
+
+      }.listRowSeparator(.visible) // мб change
+
+
+
+
+
+
+      .navigationTitle("Участники")
+        Spacer()
+
       }
-      .navigationTitle("Создатели")
       }
+
     }
 
-}
+
 
 enum Position: String{
   case TeamLead
@@ -28,6 +53,8 @@ enum Position: String{
   case Design
   case Ideas
 }
+
+
 
 
 
