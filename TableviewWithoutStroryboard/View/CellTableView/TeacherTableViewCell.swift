f1c7896c21cd53309bc52c879c966c813705f3be
Kirill <kirilldrozd12@gmail.com>
2022-04-14 23:52:29 +0300

    ...

--- TableviewWithoutStroryboard/View/CellTableView/TeacherTableViewCell.swift
+++ TableviewWithoutStroryboard/View/CellTableView/TeacherTableViewCell.swift
@@ -10,18 +10,18 @@ import SwiftUI
 import SnapKit
 
 class TableViewCell: UITableViewCell {
-
+  
   static let identifire = "CustomTableViewCell"
-
+  
   //MARK: - Создание изображения
   lazy var imageTeacher: UIImageView = {
     var image = UIImageView()
     image.image = UIImage(named: "p")
-
+    
     contentView.addSubview(image)
     return image
   }()
-
+  
   //MARK: - Создание ФИО
   lazy var nameTeacher: UILabel = {
     var label = UILabel()
@@ -37,20 +37,20 @@ class TableViewCell: UITableViewCell {
     contentView.addSubview(label)
     return label
   }()
-
-    //MARK: - Override
+  
+  //MARK: - Override
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
   }
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
-
-//MARK: - layoutSubviews
+  
+  
+  //MARK: - layoutSubviews
   override func layoutSubviews() {
     super.layoutSubviews()
-
+    
     //MARK: - Расположение изображения
     imageTeacher.snp.makeConstraints { make in
       make.width.equalTo(contentView.bounds.size.width / 4)
@@ -58,20 +58,20 @@ class TableViewCell: UITableViewCell {
       make.left.equalToSuperview().inset(4) // можно поиграться
       make.top.equalToSuperview()
     }
-
+    
     //MARK: - Расположение ФИО
     nameTeacher.snp.makeConstraints { make in
       make.leading.equalTo(imageTeacher.snp_trailingMargin).offset(contentView.bounds.size.width / 8)
       make.trailing.equalToSuperview()
       make.top.equalToSuperview().inset(2)
     }
-
+    
     //MARK: - Расположение предметов
     lessons.snp.makeConstraints { make in
       make.leading.equalTo(imageTeacher.snp_trailingMargin).offset(contentView.bounds.size.width / 9) // тоже можно поиграться
       make.trailing.equalToSuperview()
       make.top.equalTo(nameTeacher.snp_bottomMargin).offset(contentView.bounds.size.height / 50)
-
+      
     }
   }
 }
